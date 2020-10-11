eadc92d94b4d0d4949aec77f8ab91025def113fa
anurudh <dzariozyaire@gmail.com>
2020-10-11 10:33:52 +0530

    User Authentication feature Added

--- whiteboard/urls.py
+++ whiteboard/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('',include('board.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/',include('accounts.urls')),
 ]
 
 if settings.DEBUG:
